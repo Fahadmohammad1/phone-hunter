7c42cfc7a2367e05c68a68c75a25bb12612abebe
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-02-28 22:45:07 +0600

    got id in console

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // load phones
 const loadPhones = () => {
+  document.getElementById("phone-container").innerHTML = "";
   const input = document.getElementById("search-input").value;
 
   const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
@@ -15,15 +16,20 @@ const displayPhones = (phones) => {
 
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="card rounded-3 shadow boreder-0" style="width: 18rem;">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card rounded-3 shadow mx-auto boreder-0" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top p-1" alt="...">
         <div class="card-body">
             <h5 class="card-title fw-bold">${phone.phone_name}</h5>
             <h6>Brand: ${phone.brand}</h6>
-            <button class="btn btn-outline-dark">Explore</button>
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-outline-dark">Explore</button>
         </div>
     </div>
     `;
     parent.appendChild(div);
   });
+};
+
+// display phone details to UI
+const showDetails = (phoneId) => {
+  console.log(phoneId);
 };
