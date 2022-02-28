ffdec0df6e6ae384c1b3dc23148c6d564e34a18d
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-02-28 21:40:39 +0600

    data loaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+// load phones
+const loadPhones = () => {
+  const input = document.getElementById("search-input").value;
 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};
