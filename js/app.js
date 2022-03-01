a39bd8c588d0c22ca9d548a20917e92e38a23ce8
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-03-01 21:22:15 +0600

    optional chaining added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 // load phones
 const loadPhones = () => {
+  document.getElementById("spinner").style.display = "block";
   document.getElementById("details-container").innerHTML = "";
   document.getElementById("phone-container").innerHTML = "";
   const input = document.getElementById("search-input").value;
-  if (input === "") {
+
+  // error handle
+  if (input === "" || isNaN(input) === false) {
     document.getElementById("empty-error").style.display = "block";
+    document.getElementById("search-input").value = "";
+    document.getElementById("name-error").style.display = "none";
   } else {
     const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
     fetch(url)
@@ -15,10 +20,17 @@ const loadPhones = () => {
     document.getElementById("search-input").value = "";
     // empty input error
     document.getElementById("empty-error").style.display = "none";
+    // invalid name error
+    document.getElementById("name-error").style.display = "none";
   }
 };
 // display phones to UI
 const displayPhones = (phones) => {
+  // error handle
+  if (phones.length === 0) {
+    document.getElementById("name-error").style.display = "block";
+    document.getElementById("empty-error").style.display = "none";
+  }
   phones.forEach((phone) => {
     const parent = document.getElementById("phone-container");
 
@@ -34,10 +46,12 @@ const displayPhones = (phones) => {
     </div>
     `;
     parent.appendChild(div);
+    document.getElementById("spinner").style.display = "none";
   });
 };
 // load phone details
 const loadDetails = (phoneId) => {
+  document.getElementById("spinner").style.display = "block";
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
   fetch(url)
     .then((res) => res.json())
@@ -57,14 +71,16 @@ const showDetails = (singlePhone) => {
           <div class="col-12">
             <div class="card border-0 shadow-lg">
               <h3 class="text-center fs-2 fw-bold py-3">${singlePhone.name}</h3>
-              <small class="text-center mb-3">Release Date: ${singlePhone.releaseDate}</small>
+              <h6 class="text-center mb-3">Release Date: ${
+                singlePhone?.releaseDate ?? "not published"
+              }</h6>
               <img
                 src="${singlePhone.image}"
                 class="card-img-top w-25 mx-auto mt-2"
                 alt="mobile image"
               />
               <div class="card-body">
-                <table class="table table-striped table-hover">
+                <table class="table table-striped table-hover word-break">
                   <thead>
                     <tr class="fs-5 primary-color">
                       <th scope="col">Main Features:</th>
@@ -100,34 +116,40 @@ const showDetails = (singlePhone) => {
                   <tbody>
                     <tr>
                       <th scope="row">WLAN</th>
-                      <td>: ${singlePhone?.others?.WLAN}</td>
+                      <td>: ${
+                        singlePhone?.others?.WLAN ?? "result not found"
+                      }</td>
                     </tr>
                     <tr>
                       <th scope="row">Bluetooth</th>
                       <td>
-                        : ${singlePhone?.others?.Bluetooth}
+                        : ${
+                          singlePhone?.others?.Bluetooth ?? "result not found"
+                        }
                       </td>
                     </tr>
                     <tr>
                       <th scope="row">GPS</th>
-                      <td>: ${singlePhone?.others?.GPS}</td>
+                      <td>: ${
+                        singlePhone?.others?.GPS ?? "result not found"
+                      }</td>
                     </tr>
                     <tr>
                       <th scope="row">NFC</th>
                       <td>
-                        : ${singlePhone?.others?.NFC}
+                        : ${singlePhone?.others?.NFC ?? "result not found"}
                       </td>
                     </tr>
                     <tr>
                       <th scope="row">Radio</th>
                       <td>
-                        : ${singlePhone?.others?.Radio}
+                        : ${singlePhone?.others?.Radio ?? "result not found"}
                       </td>
                     </tr>
                     <tr>
                       <th scope="row">USB</th>
                       <td>
-                        : ${singlePhone?.others?.USB}
+                        : ${singlePhone?.others?.USB ?? "result not found"}
                       </td>
                     </tr>
                   </tbody>
@@ -138,4 +160,5 @@ const showDetails = (singlePhone) => {
         </div>
   `;
   detailsContainer.appendChild(div);
+  document.getElementById("spinner").style.display = "none";
 };
